73a19034d707e9833e3f957c6b91a6d6aa07e50b
torucom <torucom@mac.com>
2019-06-07 00:21:39 +0900

    Change Theme.

---
+++ init.el
@@ -0,0 +1,36 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (all-the-icons neotree emmet-mode monokai-theme)))
+ '(save-place-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme 'monokai t)
+
+(require 'all-the-icons)
+(require 'neotree)
+(setq neo-theme 'ascii)
+(setq neo-persist-show t)
+(setq neo-smart-open t)
+(global-set-key "\C-o" 'neotree-toggle)
+;;(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+;;reload
+(global-set-key
+ [f12] 'eval-buffer)
