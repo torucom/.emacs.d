a1cdbd4431b36ac296b7698b2126f5d0668bd5de
torucom <torucom@mac.com>
2019-06-10 18:23:34 +0900

    Add thme doom-thme

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
  '(doom-modeline-minor-modes nil t)
  '(package-selected-packages
    (quote
-    (markdown-mode mark use-package doom-modeline nyan-mode atom-one-dark-theme all-the-icons-dired all-the-icons neotree emmet-mode monokai-theme)))
+    (doom-themes powerline rainbow-delimiters which-key tabbar helm projectile auto-complete helm-config dashboard markdown-mode mark use-package doom-modeline nyan-mode atom-one-dark-theme all-the-icons-dired all-the-icons neotree emmet-mode monokai-theme)))
  '(save-place-mode t)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
@@ -30,7 +30,22 @@
  ;; If there is more than one, they won't work right.
  )
 
-;;use-package
+;; Set theme
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/atom-one-dark-theme/")
+;; (load-theme 'atom-one-dark t)
+(use-package doom-themes
+  :custom
+  (doom-themes-enable-italic t)
+  (doom-themes-enable-bold t)
+  :custom-face
+  (doom-modeline-bar ((t (:background "#6272a4"))))
+  :config
+  (load-theme 'doom-dracula t)
+  (doom-themes-neotree-config)
+  (doom-themes-org-config))
+
+
+;; use-package
 (eval-when-compile (require 'use-package))
 (setq use-package-verbose t)
 (setq use-package-minimum-reported-time 0.001)
@@ -39,22 +54,13 @@
 (require 'helm-config)
 (helm-mode 1)
 
-;; powerline
-(require 'powerline)
-(powerline-center-theme)
-
 ;; tabbar
 (require 'tabbar)
 (tabbar-mode 0)
 
 ;; which-key
-(add-to-list 'load-path "~/.emacs.d/elpa/which-key-20190529.114/which-key.el")
 (require 'which-key)
 (which-key-mode)
-
-;; Set theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/atom-one-dark-theme/")
-(load-theme 'atom-one-dark t)
 
 ;; neotree setting
 (require 'all-the-icons)
@@ -65,7 +71,7 @@
 (setq neo-smart-open t)
 (global-set-key [f9] 'neotree-toggle)
 
-;;doom-modeline
+;; doom-modeline
 (require 'doom-modeline)
 (doom-modeline-mode 1)
 
@@ -73,6 +79,7 @@
 (require 'nyan-mode)
 (nyan-mode)
 (nyan-start-animation)
+
 
 ;; markdown-mode
 (use-package markdown-mode
@@ -82,7 +89,34 @@
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
 
+;; dashboard
+(require 'dashboard)
+(setq dashboard-startup-banner "~/.emacs.d/images/exite.gif")
+(setq dashboard-banner-logo-title
+  (concat "GNU Emacs " emacs-version " kernel "
+    (car (split-string (shell-command-to-string "uname -r")))  " x86_64 Mac OS X "
+    (car(split-string (shell-command-to-string "sw_vers -productVersion") "-"))
+  )
+)
+
+(setq dashboard-items '((recents  . 15)
+      (bookmarks . 5)))
+
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+(dashboard-modify-heading-icons '((recents . "file-text")
+                                  (bookmarks . "book")))
+(dashboard-setup-startup-hook)
+
 ;;reload
 (global-set-key [f12] 'eval-buffer)
-
 (setq ring-bell-function 'ignore)
+
+;; ---------------  ;;
+
+;; (projectile-mode +1)
+;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(add-hook 'foo-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
