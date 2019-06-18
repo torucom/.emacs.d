2f0dfe84eadad3b7afc4b11935e77bb82cf1623f
torucom <torucom@mac.com>
2019-06-18 21:26:27 +0900

    テーマ周りを調整

--- init.el
+++ init.el
@@ -22,10 +22,27 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(doom-modeline-bar ((t (:background "#6272a4")))))
+ '(doom-modeline-bar ((t (:background "#e4007f")))))
+
+
+;; (setq make-backup-files nil);; バックアップファイルを作らない（*.~）
+;; (setq auto-save-default nil) ;; バックアップファイルを作らない（.#*）
+
+;; 自動作成するファイルの設定
+(setq backup-directory-alist '((".*" . "~/.emacs.d/bak"))) ;;バックアップファイルを”~/.emacs.d/bak” ディレクトリに作成
+(setq version-control     t)
+(setq kept-new-versions   5)
+(setq kept-old-versions   1)
+(setq delete-old-versions t)
+
+(setq auto-save-file-name-transforms   '((".*" "~/.emacs.d/tmp/" t))) ;;復帰のための自動保存ファイルは"~/.emacs.d/tmp/"に作成
+(setq create-lockfiles nil) ;;ファイルの前に’.#’が付いたロックファイルは作成しない
 
 ;; 行間調整
 (setq-default line-spacing 2)
+
+;; 閉じカッコの自動挿入
+(electric-pair-mode 1)
 
 ;; ウィンドウ移動
 (global-set-key (kbd "C-c <left>")  'windmove-left)
@@ -65,16 +82,34 @@
 ;; doom-theme設定
 (use-package doom-themes
   :custom
-  (doom-themes-enable-italic t)
+  (doom-themes-enable-italic nil)
   (doom-themes-enable-bold t)
-  :custom-face
-  (doom-modeline-bar ((t (:background "#6272a4"))))
   :config
-  (load-theme 'doom-dracula t)
+  (load-theme 'doom-Iosvkem t)
+  ;; (load-theme 'doom-city-lights t)
+  ;; (load-theme 'doom-molokai t)
+  ;; (load-theme 'doom-dracula t)
   (doom-themes-neotree-config)
-  (doom-themes-org-config))
+  (doom-themes-org-config)
+  :custom-face
+  (doom-modeline-bar ((t (:background "#e4007f"))))
+  )
 
-;; doom-modeline設定
+; (load-theme 'material t)
+
+; doom-modeline設定
+;; (use-package doom-modeline
+;;   :custom
+;;     (doom-modeline-buffer-file-name-style 'truncate-all)
+;;     (doom-modeline-icon t)
+;;     (doom-modeline-major-mode-icon t)
+;;     (doom-modeline-minor-modes t)
+;;   :hook
+;;     (after-init . doom-modeline-mode)
+;;   :config
+;;     (line-number-mode 0)
+;;     (column-number-mode 0)
+;; )
 (require 'doom-modeline)
 (doom-modeline-mode 1)
 (setq doom-modeline-icon t)
@@ -146,8 +181,6 @@
 
 ;; emmet-mode設定
 (require 'emmet-mode)
-;; emmet
-(require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; マークアップ言語全部で使う
 (add-hook 'css-mode-hook  'emmet-mode) ;; CSSにも使う
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent はスペース2個
@@ -155,10 +188,17 @@
   '(define-key emmet-mode-keymap (kbd "C-j") nil)) ;; C-j は newline のままにしておく
 (keyboard-translate ?\C-e ?\H-e) ;;C-e と Tabの被りを回避
 (define-key emmet-mode-keymap (kbd "H-e") 'emmet-expand-line) ;; C-e で展開
+(with-eval-after-load 'emmet-mode
+  (puthash "html:4s" "!!!4s+doc4[lang=ja]" (gethash "aliases" (gethash "html" emmet-snippets)))
+  (puthash "html:4t" "!!!4t+doc4[lang=ja]" (gethash "aliases" (gethash "html" emmet-snippets)))
+  (puthash "html:5" "!!!+doc[lang=ja]" (gethash "aliases" (gethash "html" emmet-snippets)))
+  (puthash "html:xml" "html[xmlns=http://www.w3.org/1999/xhtml]" (gethash "aliases" (gethash "html" emmet-snippets)))
+  (puthash "html:xs" "!!!xs+doc4[xmlns=http://www.w3.org/1999/xhtml xml:lang=ja]" (gethash "aliases" (gethash "html" emmet-snippets)))
+  )
 
 ;; neotree設定
 (require 'neotree)
-(setq neo-theme 'ascii) ;; icon, classic等もあるよ！
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 (setq neo-persist-show t) ;; delete-other-window で neotree ウィンドウを消さない
 (setq neo-smart-open t) ;; neotree ウィンドウを表示する毎に current file のあるディレクトリを表示する
 (setq neo-smart-open t)
